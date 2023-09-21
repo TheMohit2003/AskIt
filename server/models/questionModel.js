f00d6598fb7c68c0eb12042e435d6852dfed7075
Restyled.io <commits@restyled.io>
2023-09-21 08:21:33 +0000

    Restyled by prettier

--- server/models/questionModel.js
+++ server/models/questionModel.js
@@ -1,44 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Define the Question schema
 const questionSchema = new Schema(
-    {
-      title : {
-        type : String,
-        required : true,
-        maxlength : 255,
-      },
-      content : {
-        type : String,
-        required : true,
-        maxlength : 5000, // Adjust the max length as needed
-      },
-      author : {
-        type : Schema.Types.ObjectId,
-        ref : 'User',
-        required : true,
-      },
-      tags : [
-        {
-          type : String,
-          trim : true,
-          maxlength : 50,
-        },
-      ],
-      answers : [
-        {
-          type : Schema.Types.ObjectId,
-          ref : 'Answer',
-        },
-      ],
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 255,
     },
-    {
-      timestamps :
-          true, // Add timestamps (createdAt and updatedAt) to the document.
-    });
+    content: {
+      type: String,
+      required: true,
+      maxlength: 5000, // Adjust the max length as needed
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tags: [
+      {
+        type: String,
+        trim: true,
+        maxlength: 50,
+      },
+    ],
+    answers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Answer",
+      },
+    ],
+  },
+  {
+    timestamps: true, // Add timestamps (createdAt and updatedAt) to the document.
+  },
+);
 
 // Create the Question model
-const Question = mongoose.model('Question', questionSchema);
+const Question = mongoose.model("Question", questionSchema);
 
 module.exports = Question;
